ef93598baf2c56f09de3845640afb60697b2c872
priya-gode <2100030170@kluniversity.in>
2023-12-09 10:29:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import News from './News';
 
 function App() {
+  let [category,setCategory]=useState("india");
+  let [date,setDate]=useState(new Date());
+  let [articles,setArticles]=useState([]);
+  useEffect(()=>{
+    fetch(`https://newsapi.org/v2/everything?q=${category}&from=${date}&apiKey=1dd53e95d13a426cb83ff997fb6fa104`)
+    .then((response)=>response.json())
+    .then((news)=>{
+      setArticles(news.articles);
+      console.log(news.articles);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[category,date]
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+      <h1>Parso Tak</h1>
+      <input type='text' onChange={(event)=>{
+        if(event.target.value!=="")
+        {
+        setCategory(event.target.value);
+        }
+        else
+        {
+          setCategory("india");
+        }
+      }} placeholder='search news'></input>
+      <input type='text' onChange={(event)=>{
+        if(event.target.value!=="")
+        {
+        setDate(event.target.value);
+        }
+        else
+        {
+        setDate(new Date());
+        }
+      }} placeholder='Enter Date for News'></input>
       </header>
+      
+      <section className='newsarticles'> 
+      {
+        articles.length!==0?
+
+        articles.map((article)=>{
+          return(
+            <News article={article}/>
+          )
+        }):<h3>No News Found For Searched Text</h3>
+      }
+      
+     
+      </section>
     </div>
   );
 }
---
+++ src/News.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function News(props) {
+    console.log(props)
+  return (
+    <div className='news'>
+      <div className='news-img'>
+        {
+          props.article.urlToImage!==null?
+          <img src={props.article.urlToImage}></img>:
+          <img src="https://www.shutterstock.com/image-vector/house-not-available-icon-flat-260nw-1030785001.jpg"></img>
+
+        }
+        
+      </div>
+      <h2>{props.article.title}</h2>
+      <p>{props.article.description?.substring(0,100).concat("...")} <a href={props.article.url} target='_blank_'>Read more</a></p>
+     
+      <div className='source'>
+        <p>Author : {props.article.author}</p>
+        <p>{props.article.source.name}
+        </p>
+
+      </div>
+    </div>
+  )
+}
